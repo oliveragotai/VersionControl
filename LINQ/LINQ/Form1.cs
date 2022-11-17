27fcd9149daddae77cf03ab756faf8a44744e883
oliveragotai <oliver.agotai@stud.uni-corvinus.hu>
2022-11-17 22:19:36 +0100

    Create LoadData function

--- LINQ/LINQ/Form1.cs
+++ LINQ/LINQ/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,38 @@ namespace LINQ
 {
     public partial class Form1 : Form
     {
-        public Form1()
+        private List<Country> countries = new List<Country>();
+
+        void LoadData(string fileName)
+        {
+            StreamReader sr = new StreamReader(fileName);
+            sr.ReadLine();
+            while (!sr.EndOfStream)
+            {
+                string[] line = sr.ReadLine().Split(';');
+                string countryName = line[2];
+                var currentCountry = countries.Where(i => i.Name.Equals(countryName)).FirstOrDefault();
+                if (currentCountry == null)
+                {
+                    currentCountry = new Country()
+                    {
+                        ID = countries.Count + 1,
+                        Name = countryName
+                    };
+                    countries.Add(currentCountry);
+                }
+
+            }
+            sr.Close();
+        }
+
+
+
+    public Form1()
         {
             InitializeComponent();
+            LoadData("ramen.csv");
+            Console.WriteLine("breakpoint");
         }
     }
 }
