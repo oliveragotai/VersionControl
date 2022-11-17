ba6e760063587759414c82a6bfd1c3a9b2be139a
oliveragotai <oliver.agotai@stud.uni-corvinus.hu>
2022-11-17 22:41:08 +0100

    Implement reading of brands

--- LINQ/LINQ/Form1.cs
+++ LINQ/LINQ/Form1.cs
@@ -15,6 +15,7 @@ namespace LINQ
     {
         private List<Country> countries = new List<Country>();
         private List<Ramen> ramens = new List<Ramen>();
+        private List<Brand> brands = new List<Brand>();
 
         void LoadData(string fileName)
         {
@@ -24,19 +25,26 @@ namespace LINQ
             {
                 string[] line = sr.ReadLine().Split(';');
                 string countryName = line[2];
-                var country = AddCountry(countryName);
-                var ramen = new Ramen()
-                {
-                    ID = ramens.Count + 1,
-                    Brand = line[0],
-                    Name = line[1],
-                    CountryFK = country.ID,
-                    Country = country,
-                    Stars = Convert.ToDouble(line[3])
-                };
-                ramens.Add(ramen);
+                string brandName = line[0];
+                Country country = AddCountry(countryName);
+                AddBrand(brandName);
+                AddRamen(line, country);
             }
             sr.Close();
+        }
+
+        private void AddRamen(string[] line, Country country)
+        {
+            var ramen = new Ramen()
+            {
+                ID = ramens.Count + 1,
+                Brand = line[0],
+                Name = line[1],
+                CountryFK = country.ID,
+                Country = country,
+                Stars = Convert.ToDouble(line[3])
+            };
+            ramens.Add(ramen);
         }
 
         private Country AddCountry(string countryName)
@@ -52,6 +60,20 @@ namespace LINQ
                 countries.Add(currentCountry);
             }
             return currentCountry;
+        }
+
+        private void AddBrand(string brandName)
+        {
+            var currentBrand = brands.Where(i => i.Name.Equals(brandName)).FirstOrDefault();
+            if (currentBrand == null)
+            {
+                currentBrand = new Brand()
+                {
+                    ID = brands.Count + 1,
+                    Name = brandName
+                };
+                brands.Add(currentBrand);
+            }
         }
 
 
