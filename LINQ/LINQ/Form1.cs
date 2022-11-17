8d6e641e07c625d999c54da9bfeee66b09fe897b
oliveragotai <oliver.agotai@stud.uni-corvinus.hu>
2022-11-17 22:33:29 +0100

    Implement read of ramens

--- LINQ/LINQ/Form1.cs
+++ LINQ/LINQ/Form1.cs
@@ -14,6 +14,7 @@ namespace LINQ
     public partial class Form1 : Form
     {
         private List<Country> countries = new List<Country>();
+        private List<Ramen> ramens = new List<Ramen>();
 
         void LoadData(string fileName)
         {
@@ -23,24 +24,38 @@ namespace LINQ
             {
                 string[] line = sr.ReadLine().Split(';');
                 string countryName = line[2];
-                var currentCountry = countries.Where(i => i.Name.Equals(countryName)).FirstOrDefault();
-                if (currentCountry == null)
+                var country = AddCountry(countryName);
+                var ramen = new Ramen()
                 {
-                    currentCountry = new Country()
-                    {
-                        ID = countries.Count + 1,
-                        Name = countryName
-                    };
-                    countries.Add(currentCountry);
-                }
-
+                    ID = ramens.Count + 1,
+                    Brand = line[0],
+                    Name = line[1],
+                    CountryFK = country.ID,
+                    Country = country,
+                    Stars = Convert.ToDouble(line[3])
+                };
+                ramens.Add(ramen);
             }
             sr.Close();
         }
 
+        private Country AddCountry(string countryName)
+        {
+            var currentCountry = countries.Where(i => i.Name.Equals(countryName)).FirstOrDefault();
+            if (currentCountry == null)
+            {
+                currentCountry = new Country()
+                {
+                    ID = countries.Count + 1,
+                    Name = countryName
+                };
+                countries.Add(currentCountry);
+            }
+            return currentCountry;
+        }
 
 
-    public Form1()
+        public Form1()
         {
             InitializeComponent();
             LoadData("ramen.csv");
